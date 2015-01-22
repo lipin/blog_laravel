b84d2b421523feda5b1043dca4ddfd2fa8dc8442
lipin <ping.li1688@gmail.com>
2015-01-22 18:24:56 +0800

    MISC-创建模型&填充数据

---
+++ app/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+// Composer: "fzaninotto/faker": "v1.3.0"
+use Faker\Factory as Faker;
+
+class UsersTableSeeder extends Seeder {
+
+	public function run()
+	{
+		$faker = Faker::create();
+
+	
+			User::create([
+				'email'    => 'admin@admin.com',
+	            'password' => Hash::make(''),
+	            'nickname' => 'admin',
+	            'is_admin' => 1,
+
+			]);
+	}
+
+}
